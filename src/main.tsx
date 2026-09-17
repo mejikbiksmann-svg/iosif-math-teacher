c817543895fdf8ce1298fa70f96efeb337f33ab5
mejikbiksmann-svg <mejikbiksmann@gmail.com>
2026-09-17 10:56:37 +0300

    Use hand navigation with pan and zoom

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { SubjectProvider } from './context/SubjectContext'
-import { installBoardViewportPan } from './boardViewportPan'
+import { installBoardNavigation } from './boardNavigation'
 import './styles.css'
 
-installBoardViewportPan()
+installBoardNavigation()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
