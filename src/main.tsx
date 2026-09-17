6b68ce57e37615af8c553e34e02abb702ef5134e
mejikbiksmann-svg <mejikbiksmann@gmail.com>
2026-09-17 10:48:41 +0300

    Enable temporary pointer diagnostics on preview

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { SubjectProvider } from './context/SubjectContext'
+import { installPointerDiagnostics } from './pointerDiagnostics'
 import './styles.css'
+
+installPointerDiagnostics()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
